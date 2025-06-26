dab8c57eee8747b579771cb082f69cff68c54aeb
Akanksha Singh <142158559+Akankshasingh1005@users.noreply.github.com>
2025-06-26 19:54:41 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -16,7 +16,7 @@ let app = express()
 app.use(express.json())
 app.use(cookieParser())
 
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: 'https://stayfinder-n48x.onrender.com', credentials: true }));
 
 app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
